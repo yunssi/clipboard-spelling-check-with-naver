7a1d70de4e90ab0b75771be84fadf22d5acb4380
윤성구 <seonggoo.yun@nhn.com>
2017-02-23 21:30:48 +0900

    첫 번째 커밋
    첫 번째 커밋

---
+++ MainForm.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace spellchk
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.rtb_output = new System.Windows.Forms.RichTextBox();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel3 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // rtb_output
+            // 
+            this.rtb_output.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.rtb_output.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rtb_output.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.rtb_output.Location = new System.Drawing.Point(0, 0);
+            this.rtb_output.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.rtb_output.Name = "rtb_output";
+            this.rtb_output.Size = new System.Drawing.Size(382, 175);
+            this.rtb_output.TabIndex = 0;
+            this.rtb_output.Text = "";
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel3,
+            this.toolStripStatusLabel2,
+            this.toolStripStatusLabel1});
+            this.statusStrip.Location = new System.Drawing.Point(0, 153);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.statusStrip.Size = new System.Drawing.Size(382, 22);
+            this.statusStrip.SizingGrip = false;
+            this.statusStrip.TabIndex = 1;
+            this.statusStrip.Text = "statusStrip";
+            // 
+            // toolStripStatusLabel3
+            // 
+            this.toolStripStatusLabel3.ForeColor = System.Drawing.Color.Purple;
+            this.toolStripStatusLabel3.Name = "toolStripStatusLabel3";
+            this.toolStripStatusLabel3.Size = new System.Drawing.Size(71, 17);
+            this.toolStripStatusLabel3.Text = "표준어 의심";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.ForeColor = System.Drawing.Color.Green;
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(83, 17);
+            this.toolStripStatusLabel2.Text = "띄어쓰기 의심";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.ForeColor = System.Drawing.Color.Red;
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(71, 17);
+            this.toolStripStatusLabel1.Text = "맞춤법 의심";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 175);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.rtb_output);
+            this.Font = new System.Drawing.Font("맑은 고딕", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "MainForm";
+            this.Text = "클립보드에 있는 내용 맞춤법 검사하기~";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtb_output;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel3;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using System.Web;
+using System.Text.RegularExpressions;
+
+namespace spellchk
+{
+    public partial class MainForm : Form
+    {
+        ClipboardMonitor clipboardMonitor = null;
+        Regex htmlPattern = new Regex("\\\"html\\\":\\\"(.*?)\\\"", RegexOptions.Compiled);
+        Regex resultPattern = new Regex("\\\"(.*?)\\\"", RegexOptions.Compiled);
+        Regex purplePattern = new Regex("<span class='re_purple'>(.*?)</span>", RegexOptions.Compiled);
+        Regex greenPattern = new Regex("<span class='re_green'>(.*?)</span>", RegexOptions.Compiled);
+        Regex redPattern = new Regex("<span class='re_red'>(.*?)</span>", RegexOptions.Compiled);
+        Regex brTagPattern = new Regex("<br>", RegexOptions.Compiled);
+        Regex tagPattern = new Regex("<.*?>", RegexOptions.Compiled);
+        Regex specialChPattern = new Regex("(&quot;)|(&#34;)|(&amp;)|(&#38;)|(&apos;)|(&#39;)|(&lt;)|(&#60;)|(&gt;)|(&#62;)", RegexOptions.Compiled);
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            clipboardMonitor = new ClipboardMonitor(OnClipboardChanged);
+        }
+
+        private void OnClipboardChanged(object sender, ClipboardChangedEventArgs e)
+        {
+            if (e.DataObject.GetDataPresent(DataFormats.UnicodeText))
+            {
+                var inputText = e.DataObject.GetData(DataFormats.UnicodeText) as string;
+
+                if (inputText.Length > 500)
+                {
+                    return;
+                }
+
+                setFormatting(checkSpelling(inputText));
+            }
+        }
+
+        private void replaceHtmlChar(RichTextBox rtb)
+        {
+            string text = string.Empty;
+            while (true)
+            {
+                text = rtb_output.Text;
+                Match match = specialChPattern.Match(text);
+                if (!match.Success)
+                {
+                    break;
+                }
+                rtb_output.Select(match.Index, match.Length);
+                if (match.Value.Equals("&quot;") || match.Value.Equals("&#34;"))
+                {
+                    rtb_output.SelectedText = "\"";
+                }
+                else if (match.Value.Equals("&amp;") || match.Value.Equals("&#38;"))
+                {
+                    rtb_output.SelectedText = "&";
+                }
+                else if (match.Value.Equals("&apos;") || match.Value.Equals("&#39;"))
+                {
+                    rtb_output.SelectedText = "'";
+                }
+                else if (match.Value.Equals("&lt;") || match.Value.Equals("&#60;"))
+                {
+                    rtb_output.SelectedText = "<";
+                }
+                else if (match.Value.Equals("&gt;") || match.Value.Equals("&#62;"))
+                {
+                    rtb_output.SelectedText = ">";
+                }
+            }
+        }
+
+        private void setFormatting(string str)
+        {
+            rtb_output.Clear();
+            str = brTagPattern.Replace(str, "\n");
+            rtb_output.Text = str;
+
+            MatchCollection purpleMatches = purplePattern.Matches(str);
+            if (purpleMatches.Count > 0)
+            {
+                foreach (Match match in purpleMatches)
+                {
+                    rtb_output.Select(match.Index, match.Length);
+                    rtb_output.SelectionColor = Color.Purple;
+                }
+            }
+            MatchCollection greenMatches = greenPattern.Matches(str);
+            if (greenMatches.Count > 0)
+            {
+                foreach (Match match in greenMatches)
+                {
+                    rtb_output.Select(match.Index, match.Length);
+                    rtb_output.SelectionColor = Color.Green;
+                }
+            }
+            MatchCollection redMatches = redPattern.Matches(str);
+            if (redMatches.Count > 0)
+            {
+                foreach (Match match in redMatches)
+                {
+                    rtb_output.Select(match.Index, match.Length);
+                    rtb_output.SelectionColor = Color.Red;
+                }
+            }
+
+            while(true)
+            {
+                string text = rtb_output.Text;
+                Match tagMatch = tagPattern.Match(text);
+                if(!tagMatch.Success)
+                {
+                    break;
+                }
+                rtb_output.Select(tagMatch.Index, tagMatch.Length);
+                rtb_output.SelectedText = "";
+            }
+
+            replaceHtmlChar(rtb_output);
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            clipboardMonitor.Dispose();
+        }
+
+        private string checkSpelling(string text)
+        {
+            string result = string.Empty;
+            try
+            {
+                string url = "https://m.search.naver.com/p/csearch/dcontent/spellchecker.nhn?_callback=callback&q=" + HttpUtility.UrlEncode(text);
+
+                ProcessStartInfo start = new ProcessStartInfo();
+                start.FileName = @".\curl\curl.exe";
+                start.UseShellExecute = false;
+                start.RedirectStandardOutput = true;
+                start.WindowStyle = ProcessWindowStyle.Hidden;
+                start.CreateNoWindow = true;
+                start.Arguments = url;
+
+                using (Process process = Process.Start(start))
+                {
+                    using (StreamReader reader = new StreamReader(process.StandardOutput.BaseStream, Encoding.UTF8))
+                    {
+                        Match htmlMat = htmlPattern.Match(reader.ReadToEnd());
+                        result = resultPattern.Match(htmlMat.Value.Replace("\"html\"", String.Empty)).Value.Replace("\"", String.Empty);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return result;
+        }
+    }
+}
