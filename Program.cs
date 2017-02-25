4dd25fe41942ab6f0c4937bdbbfb5f66499ee3f9
윤성구 <seonggoo.yun@nhn.com>
2017-02-25 14:57:52 +0900

    단일 인스턴스만 실행되도록 수정

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -8,15 +9,27 @@ namespace spellchk
 {
     static class Program
     {
+        static Mutex mutex = new Mutex(false, "5964effa-37a0-4fbb-bdab-89adcdc25520");
+
         /// <summary>
         /// 해당 응용 프로그램의 주 진입점입니다.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+            if (!mutex.WaitOne(TimeSpan.FromSeconds(1), false))
+            {
+                MessageBox.Show("spellchk가 이미 실행 중입니다~", "", MessageBoxButtons.OK);
+                return;
+            }
+
+            try
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new MainForm());
+            }
+            finally { mutex.ReleaseMutex(); }
         }
     }
 }
